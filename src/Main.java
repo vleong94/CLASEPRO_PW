27f1e00091f49299f4925fb52f6d5c3c4c1a985c
Víctor Roberto León Guerra <victorleon.dev@gmail.com>
2025-09-30 20:50:10 +0200

    fourth commit

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,15 @@
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Fumara e campi");
-        System.out.println("Fumara e campi");
-        System.out.println("Fumara e campi");
-        System.out.println("Fumara e campi");
-        System.out.println("Fumara e campi");
-        System.out.println("Fumara e campi");
-        System.out.println("Fumara e campi");
-        System.out.println("Fumara e campi");
-        System.out.println("Fumara e campi");
+        System.out.println("Fumara");
+        System.out.println("Fumara");
+        System.out.println("Fumara");
+        System.out.println("Fumara");
+        System.out.println("Fumara");
+        System.out.println("Fumara");
+        System.out.println("Fumara");
+        System.out.println("Fumara");
+        System.out.println("Fumara");
 
 
     }
