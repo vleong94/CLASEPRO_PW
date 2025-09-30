cd7b70b4f85555acb37c20ea772167de4b0c87b7
Víctor Roberto León Guerra <victorleon.dev@gmail.com>
2025-09-30 21:01:10 +0200

    sixth commit

--- src/Main.java
+++ src/Main.java
@@ -2,16 +2,6 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Fumara");
-        System.out.print("Fumara");
-        System.out.print("Fumara");
-        System.out.print("Fumara");
-        System.out.print("Fumara");
-        System.out.print("Fumara");
-        System.out.print("Fumara");
-        System.out.print("Fumara");
-        System.out.print("Fumara");
-
-
     }
 
 }
